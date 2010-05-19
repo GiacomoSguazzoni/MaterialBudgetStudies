b79eb22f3dcbc3a6ee388810f9b9e6e398f2c766
cerati <>
2010-05-19 12:29:26 +0000

    added command line option

--- MaterialNtuplizer/countEvent.py
+++ MaterialNtuplizer/countEvent.py
@@ -1,19 +1,33 @@
-import os
-#os.system('grep passed DATADec19/res/CMSSW_*.stdout >& nEvtData.log')
-#file = open('nEvtData.log','r')
-os.system('grep passed MC/res/CMSSW_*.stdout >& nEvtMC.log')
-file = open('nEvtMC.log','r')
-tot=0;
-pas=0;
-rej=0;
-for line in file.readlines():
-    #print line
-    words = line.split()
-    #print words
-    tot=tot+int(words[4])
-    pas=pas+int(words[7])
-    rej=rej+int(words[10])
-print "total="+str(tot)
-print "passed="+str(pas)
-print "rejected="+str(rej)
-print "efficiency="+str(float(pas)/float(tot))
+#!/usr/bin/env python
+
+import os, sys
+
+from optparse import OptionParser
+parser = OptionParser(usage="usage: %prog crabdir")
+#parser.add_option("-o", "--opt", var1="value1", var2="value2")
+
+(options, args) = parser.parse_args()
+if len(args) != 1: 
+    parser.print_usage()
+    sys.exit(2)
+    
+def countevts(dir):
+    os.system('grep passed '+str(dir)+'/res/CMSSW_*.stdout >& nEvt.log')
+    file = open('nEvt.log','r')
+    tot=0;
+    pas=0;
+    rej=0;
+    for line in file.readlines():
+        #print line
+        words = line.split()
+        #print words
+        tot=tot+int(words[4])
+        pas=pas+int(words[7])
+        rej=rej+int(words[10])
+    print "total="+str(tot)
+    print "passed="+str(pas)
+    print "rejected="+str(rej)
+    print "efficiency="+str(float(pas)/float(tot))
+    os.system('rm nEvt.log')
+
+countevts(args[0])
