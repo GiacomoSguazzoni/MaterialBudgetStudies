15a89fa5d6e830e78a571ddb369462fbb8c27099
cerati <>
2010-05-19 15:15:42 +0000

    fix release name

--- MaterialNtuplizer/copyAndMergeDATA.py
+++ MaterialNtuplizer/copyAndMergeDATA.py
@@ -1,6 +1,6 @@
 import os
-pippo  = 'hadd /tmp/cerati/ntuple_conversion_CMSSW356_goodcoll7TeV.root '
-pippo2 = 'hadd /tmp/cerati/ntuple_nuclint_CMSSW356_goodcoll7TeV.root '
+pippo  = 'hadd /tmp/cerati/ntuple_conversion_CMSSW358p3_goodcoll7TeV.root '
+pippo2 = 'hadd /tmp/cerati/ntuple_nuclint_CMSSW358p3_goodcoll7TeV.root '
 done = [11,15,18,1,20,21,24,28,31,34,35,37,38,43,44,45,46,48,49,4,54,58,5,60,61,62,65,66,67,68,69,6,70,71,72,74,75,7,80,81,82,83,84,85,86,9]
 for i in range(1,87):
     if i not in done: continue
@@ -11,9 +11,9 @@ for i in range(1,87):
 #print pippo
 os.system(pippo)
 os.system(pippo2)
-os.system('rfcp /tmp/cerati/ntuple_conversion_CMSSW356_goodcoll7TeV.root $CASTOR_HOME/MaterialNtuples/ntuple_conversion_CMSSW356_goodcoll7TeV_2010-05-03.root')
-os.system('rfcp /tmp/cerati/ntuple_nuclint_CMSSW356_goodcoll7TeV.root $CASTOR_HOME/MaterialNtuples/ntuple_nuclint_CMSSW356_goodcoll7TeV_2010-05-03.root')
+os.system('rfcp /tmp/cerati/ntuple_conversion_CMSSW358p3_goodcoll7TeV.root $CASTOR_HOME/MaterialNtuples/ntuple_conversion_CMSSW358p3_goodcoll7TeV_2010-05-03.root')
+os.system('rfcp /tmp/cerati/ntuple_nuclint_CMSSW358p3_goodcoll7TeV.root $CASTOR_HOME/MaterialNtuples/ntuple_nuclint_CMSSW358p3_goodcoll7TeV_2010-05-03.root')
 #os.system('rm /tmp/cerati/*.root')
 #for i in range(1,204):
-#    os.system('rfrm /castor/cern.ch/cms/store/caf/user/cerati/ntuple_conversion_CMSSW336p3_goodcoll900GeV_'+str(i)+'.root')
+#    os.system('rfrm /castor/cern.ch/cms/store/caf/user/cerati/ntuple_conversion_CMSSW358p3_goodcoll900GeV_'+str(i)+'.root')
 
--- MaterialNtuplizer/copyAndMergeMC.py
+++ MaterialNtuplizer/copyAndMergeMC.py
@@ -1,7 +1,7 @@
 import os
-pippo  = 'hadd /tmp/cerati/ntuple_conversion_CMSSW356_minbias7TeV.root '
-pippo1 = 'hadd /tmp/cerati/ntuple_conversion_CMSSW356_minbias7TeV_hit.root '
-pippo2 = 'hadd /tmp/cerati/ntuple_nuclint_CMSSW356_minbias7TeV.root '
+pippo  = 'hadd /tmp/cerati/ntuple_conversion_CMSSW358p3_minbias7TeV.root '
+pippo1 = 'hadd /tmp/cerati/ntuple_conversion_CMSSW358p3_minbias7TeV_hit.root '
+pippo2 = 'hadd /tmp/cerati/ntuple_nuclint_CMSSW358p3_minbias7TeV.root '
 #undone = [10,13,19,26,29,30,32,38,43,44,47,49,58,62,66,69,72,75,78,80,84,86,88,91,93,95,100,106,111,112]
 for i in range(1,114):
 #    if i in undone: continue
@@ -19,9 +19,9 @@ for i in range(1,114):
 os.system(pippo)
 os.system(pippo1)
 os.system(pippo2)
-os.system('rfcp /tmp/cerati/ntuple_conversion_CMSSW356_minbias7TeV.root $CASTOR_HOME/MaterialNtuples/ntuple_conversion_CMSSW356_minbias7TeV_2010-05-10.root')
-os.system('rfcp /tmp/cerati/ntuple_conversion_CMSSW356_minbias7TeV_hit.root $CASTOR_HOME/MaterialNtuples/ntuple_conversion_CMSSW356_minbias7TeV_hit_2010-05-10.root')
-os.system('rfcp /tmp/cerati/ntuple_nuclint_CMSSW356_minbias7TeV.root $CASTOR_HOME/MaterialNtuples/ntuple_nuclint_CMSSW356_minbias7TeV_2010-05-10.root')
+os.system('rfcp /tmp/cerati/ntuple_conversion_CMSSW358p3_minbias7TeV.root $CASTOR_HOME/MaterialNtuples/ntuple_conversion_CMSSW358p3_minbias7TeV_2010-05-10.root')
+os.system('rfcp /tmp/cerati/ntuple_conversion_CMSSW358p3_minbias7TeV_hit.root $CASTOR_HOME/MaterialNtuples/ntuple_conversion_CMSSW358p3_minbias7TeV_hit_2010-05-10.root')
+os.system('rfcp /tmp/cerati/ntuple_nuclint_CMSSW358p3_minbias7TeV.root $CASTOR_HOME/MaterialNtuples/ntuple_nuclint_CMSSW358p3_minbias7TeV_2010-05-10.root')
 #os.system('rm /tmp/cerati/*.root')
 #for i in range(1,204):
-#    os.system('rfrm /castor/cern.ch/cms/store/caf/user/cerati/ntuple_conversion_CMSSW336p3_minbias900GeV_'+str(i)+'.root')
+#    os.system('rfrm /castor/cern.ch/cms/store/caf/user/cerati/ntuple_conversion_CMSSW358p3_minbias900GeV_'+str(i)+'.root')
