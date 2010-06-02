99231d8e83619e8916288c063c8acf303aad2deb
cerati <>
2010-06-02 11:58:12 +0000

    *** empty log message ***

--- MaterialNtuplizer/copyDATA.py
+++ MaterialNtuplizer/copyDATA.py
@@ -1,7 +1,7 @@
 import commands
 import os
 
-dir = "GoodCollSkim2"
+dir = "MCtmp"
 os.system("mkdir /tmp/cerati/"+dir)
 tmp = commands.getoutput("nsls Tmp/"+dir)
 tmp2 = tmp.split()
@@ -9,3 +9,5 @@ for i in tmp2:
     os.system("stager_get -M $CASTOR_HOME/Tmp/"+dir+"/"+i)
 for i in tmp2:
     os.system("rfcp $CASTOR_HOME/Tmp/"+dir+"/"+i+" /tmp/cerati/"+dir)
+#os.system("hadd conv.root /tmp/cerati/*conv*.root")
+#os.system("hadd nucl.root /tmp/cerati/*nucl*.root")
