3e78e42d1adff5f781cf1a5781bb48dc4b2c89b5
cerati <>
2010-06-02 11:56:28 +0000

    updates from maxime and adding branch to count events

--- MaterialNtuplizer/src/NuclIntNtuplizer.cc
+++ MaterialNtuplizer/src/NuclIntNtuplizer.cc
@@ -5,15 +5,15 @@
 // 
 /**\class NuclIntNtuplizer NuclIntNtuplizer.cc Tests/NuclIntTest/src/NuclIntNtuplizer.cc
 
- Description: <one line class summary>
+Description: <one line class summary>
 
- Implementation:
-     <Notes on implementation>
+Implementation:
+<Notes on implementation>
 */
 //
 // Original Author:  Giuseppe Cerati
 //         Created:  Wed Aug 19 15:39:10 CEST 2009
-// $Id: NuclIntNtuplizer.cc,v 1.4 2010/05/10 08:36:01 cerati Exp $
+// $Id: NuclIntNtuplizer.cc,v 1.5 2010/05/19 15:12:58 cerati Exp $
 //
 //
 
@@ -29,7 +29,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
+//#include "FWCore/ParameterSet/interface/InputTag.h"
 
 #include "DataFormats/ParticleFlowReco/interface/PFDisplacedVertex.h"
 #include "DataFormats/ParticleFlowReco/interface/PFDisplacedVertexFwd.h"
@@ -50,6 +50,7 @@
 typedef struct { 
   int run, event;
   int isAssoc;
+  bool isNuclSim, isKSim;
   float pt, phi, theta;
   float ptOut, phiOut, thetaOut, mOut;
   int nOut;
@@ -64,8 +65,10 @@ typedef struct {
 typedef struct { 
   int run, event;
   int isAssoc;
+  bool isNuclSim, isKSim;
   float pt, phi, theta;
   float ptOut, phiOut, thetaOut, mOut, angle;
+  float mK;
   int nOut, nOutTkStep67Good, nOutTkStep67Poor;
   bool isTherePrimaryTrack, isThereMergedTrack;
   bool isNucl, isNuclLoose, isNuclKink;
@@ -80,6 +83,11 @@ typedef struct {
 
 } RECOTOSIM;
 
+typedef struct {
+  int run, event;
+  unsigned int lumi;
+} EVT;
+
 class NuclIntNtuplizer : public edm::EDAnalyzer {
 public:
   explicit NuclIntNtuplizer(const edm::ParameterSet&);
@@ -90,6 +98,8 @@ private:
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
   bool isNuclInt(const TrackingVertex&) const;  
+  bool isKDecay(const TrackingVertex&) const;  
+  float getKMass(const reco::PFDisplacedVertex&) const;
 
   TFile * file;
   std::string outfile;
@@ -99,6 +109,8 @@ private:
   SIMTORECO s2rbranch;
   RECOTOSIM r2sbranch;
   TTree *ntupleS2R,*ntupleR2S;
+  EVT evtbranch;
+  TTree *ntupleEvt;
 };
 
 NuclIntNtuplizer::NuclIntNtuplizer(const edm::ParameterSet& iConfig) :
@@ -117,9 +129,21 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
 
   if (prints) cout << "new evt" << endl;
 
+  edm::EventID eventId = iEvent.id();
+  int runNer = iEvent.run();
+  int eventNumber = eventId.event();
+  unsigned int lumi_block = eventId.luminosityBlock();
+  evtbranch.run = runNer;
+  evtbranch.event = eventNumber;
+  evtbranch.lumi = lumi_block;
+  ntupleEvt->Fill();
+
   s2rbranch.run=iEvent.run();
   s2rbranch.event=iEvent.id().event();
   s2rbranch.isAssoc =0;
+  s2rbranch.isNuclSim =0;
+  s2rbranch.isKSim =0;
+
   s2rbranch.pt =0;
   s2rbranch.phi =0;
   s2rbranch.theta =0;
@@ -141,6 +165,7 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
   s2rbranch.isNuclLoose =0;
   s2rbranch.isNuclKink =0;
 
+
   s2rbranch.isK0 =0;
   s2rbranch.isLambda =0;
   s2rbranch.isLambdaBar =0;
@@ -157,12 +182,17 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
   r2sbranch.run=iEvent.run();
   r2sbranch.event=iEvent.id().event();
   r2sbranch.isAssoc =0;
+
+  r2sbranch.isNuclSim =0;
+  r2sbranch.isKSim =0;
+
   r2sbranch.pt =0;
   r2sbranch.phi =0;
   r2sbranch.theta =0;
   r2sbranch.ptOut =0;
   r2sbranch.phiOut =0;
   r2sbranch.thetaOut =0;
+  r2sbranch.mK =0;
   r2sbranch.mOut =0;
   r2sbranch.nOutTkStep67Good =0;
   r2sbranch.nOutTkStep67Poor =0;
@@ -211,15 +241,17 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
   reco::Vertex the_pvtx;
   bool valid_pvtx = false;
   if (!vertexHandle->empty()){
-      the_pvtx = *(vertexHandle->begin());
-      valid_pvtx = true;
+    the_pvtx = *(vertexHandle->begin());
+    valid_pvtx = true;
   }
 
   Handle<TrackingVertexCollection> SimVtx;
+
   if (simulation) {
     if (prints) cout << "get sim stuff" << endl;
     //get sim info
-    iEvent.getByLabel("mergedtruth","MergedTrackTruth",SimVtx);
+    bool found = iEvent.getByLabel("mergedtruth","MergedTrackTruth",SimVtx);
+    simulation = found && simulation;
     if (hitassoc) {
       //iSetup.get<TrackAssociatorRecord>().get("TrackAssociatorByHits",theAssociator);
       //iEvent.getByLabel("mergedtruth","MergedTrackTruth",TPCollectionH);
@@ -233,7 +265,10 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
 
       //check if it is nucl int
       if (iVtx->nSourceTracks()<1) continue; 
-      if (isNuclInt(*iVtx)==false) continue;
+      bool isNuclSim = isNuclInt(*iVtx);
+      bool isKSim = isKDecay(*iVtx);
+
+      if (!isNuclSim && !isKSim) continue;
 
       if (prints) cout << "fill mc" << endl;
       math::XYZVectorD momIncSim = (*iVtx->sourceTracks_begin())->momentum();
@@ -243,6 +278,9 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
 	momOutSim += (*tpOut)->p4();
 	nTrackingParticles++;
       }
+
+      s2rbranch.isNuclSim = isNuclSim;
+      s2rbranch.isKSim = isKSim;
 
       s2rbranch.x =iVtx->position().x();
       s2rbranch.y =iVtx->position().y();
@@ -275,17 +313,6 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
       s2rbranch.isConvLoose =0;
       s2rbranch.isLooper =0;
       s2rbranch.isFake =0;
-
-      r2sbranch.tkPt = std::vector<float>();
-      r2sbranch.tkEta = std::vector<float>();
-      r2sbranch.tkDxy = std::vector<float>();
-      r2sbranch.tkDz = std::vector<float>();
-      r2sbranch.tkRho = std::vector<float>();
-      r2sbranch.tkHits = std::vector<int>();
-      r2sbranch.tkAlgo = std::vector<int>();
-      r2sbranch.tkOuter = std::vector<int>();
-      r2sbranch.tkPrimary = std::vector<bool>();
-      r2sbranch.tkSecondary = std::vector<bool>();
 
       //look for matching reco conversion
       if (prints) cout << "look for match" << endl;
@@ -379,6 +406,7 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     r2sbranch.phiOut = momOutRec.Phi();
     r2sbranch.thetaOut = momOutRec.Theta();
     r2sbranch.mOut = momOutRec.mass();
+    if (rni->isKplus_Loose() || rni->isKminus_Loose()) r2sbranch.mK = getKMass(*rni);
     r2sbranch.angle = angle;
 
     r2sbranch.isTherePrimaryTrack = rni->isTherePrimaryTracks();
@@ -440,6 +468,8 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     r2sbranch.nOutTkStep67Poor =  tkStep67Poor;
 
     r2sbranch.isAssoc = 0;
+    r2sbranch.isNuclSim =0;
+    r2sbranch.isKSim =0;
     r2sbranch.deltapt = 0;
     r2sbranch.deltaphi = 0;
     r2sbranch.deltatheta = 0;
@@ -460,6 +490,9 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     double deltaPt_InSim_OutRec = 999;
     double deltaPhi_InSim_OutRec = 999;
     double deltaTheta_InSim_OutRec = 999;
+    bool isNuclSim = 0;
+    bool isKSim = 0;
+
 
     bool associated = false;
     if (simulation) {
@@ -468,7 +501,11 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
       for (vector<TrackingVertex>::const_iterator iVtx=SimVtx->begin(); iVtx !=SimVtx->end(); ++iVtx ) {
 	//check if it is nucl int
 	if (iVtx->nSourceTracks()<1) continue; 
-	if (isNuclInt(*iVtx)==false) continue;
+
+        isNuclSim = isNuclInt(*iVtx);
+	isKSim = isKDecay(*iVtx);
+
+	if (!isNuclSim && !isKSim ) continue;
 
 	math::XYZVectorD momIncSim = (*iVtx->sourceTracks_begin())->momentum();
 	math::XYZTLorentzVectorD momOutSim(0,0,0,0);
@@ -501,6 +538,8 @@ void NuclIntNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
       if (associated) {
 	if (prints) cout << "associated" << endl;
 	r2sbranch.isAssoc =1;
+	r2sbranch.isNuclSim = isNuclSim;
+	r2sbranch.isKSim = isKSim;
 	r2sbranch.deltapt =deltaPt;
 	r2sbranch.deltaphi =deltaPhi;
 	r2sbranch.deltatheta =deltaTheta;
@@ -528,10 +567,17 @@ void NuclIntNtuplizer::beginJob() {
   const bool oldAddDir = TH1::AddDirectoryStatus();
   TH1::AddDirectory(true);
 
+  ntupleEvt = new TTree("ntupleEvt", "evt");
+  ntupleEvt->Branch("run", &(evtbranch.run),"run/I");
+  ntupleEvt->Branch("event", &(evtbranch.event),"event/I");
+  ntupleEvt->Branch("lumi", &(evtbranch.lumi),"lumi/I");
+
   ntupleS2R = new TTree("ntupleS2R","sim2reco");
   ntupleS2R->Branch("run",&(s2rbranch.run),"run/I");
   ntupleS2R->Branch("event",&(s2rbranch.event),"event/I");
   ntupleS2R->Branch("isAssoc",&(s2rbranch.isAssoc),"isAssoc/I");
+  ntupleS2R->Branch("isNuclSim",&(s2rbranch.isNuclSim),"isNuclSim/B");
+  ntupleS2R->Branch("isKSim",&(s2rbranch.isKSim),"isKSim/B");
   ntupleS2R->Branch("pt",&(s2rbranch.pt),"pt/F");
   ntupleS2R->Branch("phi",&(s2rbranch.phi),"phi/F");
   ntupleS2R->Branch("theta",&(s2rbranch.theta),"theta/F");
@@ -567,6 +613,8 @@ void NuclIntNtuplizer::beginJob() {
   ntupleR2S->Branch("run",&(r2sbranch.run),"run/I");
   ntupleR2S->Branch("event",&(r2sbranch.event),"event/I");
   ntupleR2S->Branch("isAssoc",&(r2sbranch.isAssoc),"isAssoc/I");
+  ntupleR2S->Branch("isNuclSim",&(r2sbranch.isNuclSim),"isNuclSim/B");
+  ntupleR2S->Branch("isKSim",&(r2sbranch.isKSim),"isKSim/B");
   ntupleR2S->Branch("pt",&(r2sbranch.pt),"pt/F");
   ntupleR2S->Branch("phi",&(r2sbranch.phi),"phi/F");
   ntupleR2S->Branch("theta",&(r2sbranch.theta),"theta/F");
@@ -574,6 +622,7 @@ void NuclIntNtuplizer::beginJob() {
   ntupleR2S->Branch("phiOut",&(r2sbranch.phiOut),"phiOut/F");
   ntupleR2S->Branch("thetaOut",&(r2sbranch.thetaOut),"thetaOut/F");
   ntupleR2S->Branch("mOut",&(r2sbranch.mOut),"mOut/F");
+  ntupleR2S->Branch("mK",&(r2sbranch.mK),"mK/F");
   ntupleR2S->Branch("angle",&(r2sbranch.angle),"angle/F");
 
   ntupleR2S->Branch("nOut",&(r2sbranch.nOut),"nOut/I");
@@ -639,6 +688,7 @@ bool NuclIntNtuplizer::isNuclInt(const TrackingVertex& v ) const {
   //TLorentzVector momentum_out = momentum(v, PFDisplacedVertex::T_FROM_VERTEX);
   //int nCharged = 0;
   
+  bool bK = false;
   bool bK0s = false;
   bool bLambda = false;
   bool bGamma = false;
@@ -648,6 +698,7 @@ bool NuclIntNtuplizer::isNuclInt(const TrackingVertex& v ) const {
     bGamma = ((**simMother).pdgId() == 22);
     bK0s = ((**simMother).pdgId() == 310 || (**simMother).pdgId() == -310);
     bLambda = ((**simMother).pdgId() == 3122 || (**simMother).pdgId() == -3122);
+    bK = ((**simMother).pdgId() == 321 || (**simMother).pdgId() == -321);
   }
 
   vector<int> pdgIds;
@@ -668,14 +719,83 @@ bool NuclIntNtuplizer::isNuclInt(const TrackingVertex& v ) const {
   if (bK0sDecay) return false;
 
   bool bLambdaDecay = bLambda &&  v.nDaughterTracks() == 2 && ((pdgIds[0] == -211 && pdgIds[1] == 2212)|| 
-      (pdgIds[0] == 2212 && pdgIds[1] == -211)||(pdgIds[0] == 211 && pdgIds[1] == -2212)||(pdgIds[0] == -2212 && pdgIds[1] == 211));
+							       (pdgIds[0] == 2212 && pdgIds[1] == -211)||(pdgIds[0] == 211 && pdgIds[1] == -2212)||(pdgIds[0] == -2212 && pdgIds[1] == 211));
   if (bLambdaDecay) return false;
 
   bool bConvers = bGamma;
   if (bConvers) return false;
 
+  bK = bK && (pdgIds[0] == 13 || pdgIds[0] == -13) && v.nDaughterTracks() == 1;
+  if (bK) return false;
+
   return true;
 }
+
+
+
+bool NuclIntNtuplizer::isKDecay(const TrackingVertex& v ) const {
+  using namespace std;
+  
+  bool bKPlus = false;
+  bool bKMinus = false;
+
+  for (TrackingVertex::tp_iterator simMother = v.sourceTracks_begin();simMother != v.sourceTracks_end();++simMother){
+    bKPlus = bKPlus || (**simMother).pdgId() == 321;
+    bKMinus = bKMinus || (**simMother).pdgId() == -321;
+  }
+
+  vector<int> pdgIds;
+  int i = -1;
+  for (TrackingVertex::tp_iterator simDaughter = v.daughterTracks_begin();simDaughter != v.daughterTracks_end();++simDaughter){
+    pdgIds.push_back((**simDaughter).pdgId());        
+    i++;
+    //    if (bK) cout << "pdgIds[" << i << "] = " << (**simDaughter).pdgId();
+
+  }
+  //  if (bK) cout << "" << endl;
+  
+
+  bKPlus = bKPlus && pdgIds[0]==-13;
+  bKMinus = bKMinus && pdgIds[0]==13;
+  if (bKPlus || bKMinus) return true;
+  return false;
+
+}
+
+
+
+
+
+float NuclIntNtuplizer::getKMass(const reco::PFDisplacedVertex& v) const {
+
+  using namespace reco;
+
+  math::XYZVector  trkInit = v.refittedTracks()[1].momentum(), 
+    trkFinal = v.refittedTracks()[0].momentum();
+
+  if (v.trackTypes()[0] == PFDisplacedVertex::T_TO_VERTEX)
+    trkInit = v.refittedTracks()[0].momentum(),
+      trkFinal =  v.refittedTracks()[1].momentum();
+
+
+  math::XYZVector trkNeutre(trkInit.x()-trkFinal.x(),  trkInit.y()-trkFinal.y(),
+			    trkInit.z()-trkFinal.z());
+
+  double muon_mass2 = 0.106*0.106;
+
+  double Ec = sqrt(muon_mass2 + trkFinal.Mag2());
+  double En = sqrt(0*0        + trkNeutre.Mag2());
+
+
+
+  math::XYZTLorentzVectorD trkMuNu(trkInit.x(), trkInit.y(), trkInit.z(), Ec+En);
+  double massMuNu = trkMuNu.M();
+
+  return massMuNu;
+
+}
+
+
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(NuclIntNtuplizer);
@@ -686,38 +806,38 @@ DEFINE_FWK_MODULE(NuclIntNtuplizer);
 /*
 
 
-  Handle<SimToRecoCollection> hSimToReco;
-  Handle<RecoToSimCollection> hRecoToSim;
+Handle<SimToRecoCollection> hSimToReco;
+Handle<RecoToSimCollection> hRecoToSim;
 
-  iEvent.getByLabel(simToRecoLabel_, hSimToReco);
-  iEvent.getByLabel(recoToSimLabel_, hRecoToSim);
+iEvent.getByLabel(simToRecoLabel_, hSimToReco);
+iEvent.getByLabel(recoToSimLabel_, hRecoToSim);
 
-  SimToRecoCollection simToReco = *(hSimToReco.product());
-  RecoToSimCollection recoToSim = *(hRecoToSim.product());
+SimToRecoCollection simToReco = *(hSimToReco.product());
+RecoToSimCollection recoToSim = *(hRecoToSim.product());
 
 
   
-  VertexCollection* ptrVertex = new VertexCollection();
-  const PFDisplacedVertexCollection displacedVertexColl = *(displacedVertices.product());
+VertexCollection* ptrVertex = new VertexCollection();
+const PFDisplacedVertexCollection displacedVertexColl = *(displacedVertices.product());
 
-  for (PFDisplacedVertexCollection::size_type i=0; i<displacedVertexColl.size(); i++){
+for (PFDisplacedVertexCollection::size_type i=0; i<displacedVertexColl.size(); i++){
 
-    ptrVertex->push_back((Vertex) displacedVertexColl[i]);
+ptrVertex->push_back((Vertex) displacedVertexColl[i]);
     
 
-  }
+}
 
-  Handle<VertexCollection> vertices(ptrVertex, displacedVertices.provenance());
-
-
+Handle<VertexCollection> vertices(ptrVertex, displacedVertices.provenance());
 
 
-  VertexRecoToSimCollection r2s = 
-    theVertexAssociator->associateRecoToSim(vertices, VPCollection,
-					    iEvent, recoToSim);
 
-  VertexSimToRecoCollection s2r = 
-    theVertexAssociator->associateSimToReco(vertices, VPCollection,
-					    iEvent, simToReco);
+
+VertexRecoToSimCollection r2s = 
+theVertexAssociator->associateRecoToSim(vertices, VPCollection,
+iEvent, recoToSim);
+
+VertexSimToRecoCollection s2r = 
+theVertexAssociator->associateSimToReco(vertices, VPCollection,
+iEvent, simToReco);
 
 */
